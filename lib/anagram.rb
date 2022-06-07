ec1918a58649df6c7d9aff983ac9a1eebab0f45e
brian corker <bcorker123@gmail.com>
2022-06-07 17:40:15 -0400

    commit tree 58eb8d68430234d2e3319f8a4e8f818a6c2d92a6

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,16 @@
 # Your code goes here!
+class Anagram
+    def initialize(word)
+        @word = word
+    end
+
+    def match(anagram_array)
+        return_array = []
+        anagram_array.map do |anagram|
+            if @word.chars.sort == anagram.chars.sort
+                return_array << anagram
+            end
+        end
+        return_array
+    end
+end 
